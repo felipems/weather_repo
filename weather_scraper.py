29e35512540469959934aab480d0fa28a58b57ec
felipems <felipems@felipems-mbp-3.local>
2017-03-16 20:22:11 -0700

    converted to space indent

--- weather_scraper.py
+++ weather_scraper.py
@@ -15,23 +15,23 @@ import urllib
 import re
 def main():
 
-	r = urllib.urlopen('http://10.0.0.55/livedata.htm').read()
-	soup = BeautifulSoup(r, 'lxml')
-	print "done"
-	
-	data_points = soup.find_all("input", class_="item_2")
+    r = urllib.urlopen('http://10.0.0.55/livedata.htm').read()
+    soup = BeautifulSoup(r, 'lxml')
+    print "done"
+    
+    data_points = soup.find_all("input", class_="item_2")
 
-	for elem in data_points:
-		# print elem
-		e_list =  str(elem).split("=")
-		print re.search('.*"' ,e_list[4]).group(0).replace('"', "")
-		print re.search('.*"', e_list[-1]).group(0).replace('"', "")
-		print ""
+    for elem in data_points:
+        # print elem
+        e_list =  str(elem).split("=")
+        print re.search('.*"' ,e_list[4]).group(0).replace('"', "")
+        print re.search('.*"', e_list[-1]).group(0).replace('"', "")
+        print ""
 
 
 
 if __name__ == "__main__":
-	sys.exit(main()) 
+    sys.exit(main()) 
 
 
 
